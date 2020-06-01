bf0317a84d3868360b667de718ee2bc9254a2d5b
SparXFusion <s2234nadar@gmail.com>
2020-06-01 19:21:19 +0530

    RMX1831: nuke old RIL blobs

--- lucifer-vendor.mk
+++ lucifer-vendor.mk
@@ -5,6 +5,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/lucifer/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/realme/lucifer/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/lucifer/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
-    vendor/realme/lucifer/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
-    vendor/realme/lucifer/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmtk-ril.so 
+    vendor/realme/lucifer/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so 
+
     
