26c88b0c10503eac5ef97b96346cc4efba56084e
SparXFusion <s2234nadar@gmail.com>
2020-08-01 13:16:02 +0530

    RMX1831: add prebuilt modules

--- lucifer-vendor.mk
+++ lucifer-vendor.mk
@@ -7,4 +7,8 @@ PRODUCT_COPY_FILES += \
     vendor/realme/lucifer/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/lucifer/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
     vendor/realme/lucifer/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so \
-    vendor/realme/lucifer/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so 
+    vendor/realme/lucifer/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so \
+    vendor/realme/lucifer/proprietary/vendor/lib/modules/bt_drv.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules_prebuilt/bt_drv.ko \
+    vendor/realme/lucifer/proprietary/vendor/lib/modules/wlan_drv_gen3.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules_prebuilt/wlan_drv_gen3.ko \
+    vendor/realme/lucifer/proprietary/vendor/lib/modules/wmt_chrdev_wifi.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules_prebuilt/wmt_chrdev_wifi.ko \
+    vendor/realme/lucifer/proprietary/vendor/lib/modules/wmt_drv.ko:$(TARGET_COPY_OUT_VENDOR)/lib/modules_prebuilt/wmt_drv.ko 
